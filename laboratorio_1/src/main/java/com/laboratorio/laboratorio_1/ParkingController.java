3c6ed185a3bb0386766919a853d57c949eb2841e
Toshiro Escobedo Correa <aescobedoc4@upao.edu.pe>
2023-04-16 14:50:06 -0500

    feat: Add endpoint to add car: Agregar el endpoint para agregar un auto al parqueo

--- laboratorio_1/src/main/java/com/laboratorio/laboratorio_1/ParkingController.java
+++ laboratorio_1/src/main/java/com/laboratorio/laboratorio_1/ParkingController.java
@@ -4,10 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -47,6 +51,11 @@ public class ParkingController {
 	}
 
     //Post car
+    @PostMapping("/cars")
+	public ResponseEntity<Car> addCar(@RequestBody Car car){
+		this.parkingService.addCar(car);
+		return new ResponseEntity<>(car,HttpStatus.CREATED);
+	}
 
     //Deleted car by license plate
 
