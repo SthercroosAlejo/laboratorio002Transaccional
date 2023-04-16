2ddec9cfd2c22d6f1529e74f3f0ac2a0ad8c3c5c
Toshiro Escobedo Correa <aescobedoc4@upao.edu.pe>
2023-04-16 14:29:05 -0500

    feat: definir endpoints para permitir que el cliente realice acciones en el estacionamiento de vehículos

--- laboratorio_1/src/main/java/com/laboratorio/laboratorio_1/ParkingController.java
+++ laboratorio_1/src/main/java/com/laboratorio/laboratorio_1/ParkingController.java
@@ -1,6 +1,24 @@
 package com.laboratorio.laboratorio_1;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 // handler de la API
+
+@RestController
+@RequestMapping("/parking")
 public class ParkingController {
-    
+    //Get cars
+
+    //Get car by license plate
+
+    //Get car by color
+
+    //Post car
+
+    //Deleted car by license plate
+
+    //Post entry time
+
+    //Post exit time
 }
