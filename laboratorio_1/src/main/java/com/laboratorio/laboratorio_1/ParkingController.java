a36b1de6b4e5435360b2dbfd7b84aac515180ad7
Toshiro Escobedo Correa <aescobedoc4@upao.edu.pe>
2023-04-16 14:48:42 -0500

    feat: Add endpoint to get cars by color: Agregar el endpoint para buscar autos por su color.

--- laboratorio_1/src/main/java/com/laboratorio/laboratorio_1/ParkingController.java
+++ laboratorio_1/src/main/java/com/laboratorio/laboratorio_1/ParkingController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -34,7 +36,15 @@ public class ParkingController {
 		return ResponseEntity.notFound().build();
 	}
 
-    //Get car by color
+    //Get cars by color
+    @GetMapping("/cars/color")
+	public ResponseEntity<List<Car>> getCarsByColor(@RequestParam(required = false) String color){
+		if (color != null){
+			List<Car> carsByColor = this.parkingService.getCarsByColor(color);
+			return ResponseEntity.ok(carsByColor);
+		}
+		return ResponseEntity.ok(this.parkingService.getAllCars());
+	}
 
     //Post car
 
