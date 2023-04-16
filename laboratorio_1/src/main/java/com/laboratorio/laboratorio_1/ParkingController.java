2b211f65c604b824fafcca07cec13d0bb121b1fc
Toshiro Escobedo Correa <aescobedoc4@upao.edu.pe>
2023-04-16 14:46:07 -0500

    feat: Add endpoint to get all cars: Agrega el endpoint para obtener la lista de todos los autos estacionados.

--- laboratorio_1/src/main/java/com/laboratorio/laboratorio_1/ParkingController.java
+++ laboratorio_1/src/main/java/com/laboratorio/laboratorio_1/ParkingController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
 // handler de la API
 @RestController
@@ -14,6 +17,10 @@ public class ParkingController {
 	private ParkingService parkingService;
 
     //Get cars
+    @GetMapping("/cars")
+	public ResponseEntity<List<Car>> getAllCars(){
+		return ResponseEntity.ok(this.parkingService.getAllCars());
+	}
 
     //Get car by license plate
 
